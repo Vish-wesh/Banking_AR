7cafd6d2d8226bcf79fbd39d1281074e6e81bed6
vdagrawal08 <vdagrawal08@gmail.com>
2020-06-26 16:54:40 +0530

    asset folder commit

---
+++ Assets/Datafiles/scripts/VButton_Script.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VButton_Script : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject cube, cubeGo;
+    VirtualButtonBehaviour vrb;
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cube.SetActive(false);
+        cubeGo.SetActive(true);
+    }
+
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cube.SetActive(true);
+        cubeGo.SetActive(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vrb = GetComponentInChildren<VirtualButtonBehaviour>();
+        vrb.RegisterEventHandler(this);
+        cube.SetActive(true);
+        cubeGo.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
+
+
